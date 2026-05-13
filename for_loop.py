1ef64b20a96ff6c4772e48d84821e30f66d48974
indrek-png <indrek.noor@tktk.ee>
2026-05-13 13:47:31 +0300

    Implement number printing programs in for_loop.py
    Add programs to print numbers in different ranges.

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+""" for i in range(1, 11):
+    print(str(i) + ". I will skateboard in the halls.") """
+
+#Kirjutage programm, mis kuvab konsoolis arvud ühest viieni.
+""" for i in range(1,6):
+    print(str(i) + ".") """
+
+#Kirjutage programm, mis kuvab konsoolis arvud viiest uheni (tagurpidi).
+
+for i in range(5, 0, -1):
+    print(i)
